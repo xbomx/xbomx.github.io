5700cc23314475afdf9190b3e46dfd569a595d9f
xbomx <49806281+xbomx@users.noreply.github.com>
2019-04-20 14:34:04 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form Pendaftaran</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript">
+	$(document).ready(function() {
+	  var panelOne = $('.form-panel.two').height(),
+		panelTwo = $('.form-panel.two')[0].scrollHeight;
+
+	  $('.form-panel.two').not('.form-panel.two.active').on('click', function(e) {
+		e.preventDefault();
+
+		$('.form-toggle').addClass('visible');
+		$('.form-panel.one').addClass('hidden');
+		$('.form-panel.two').addClass('active');
+		$('.form').animate({
+		  'height': panelTwo
+		}, 200);
+	  });
+
+	  $('.form-toggle').on('click', function(e) {
+		e.preventDefault();
+		$(this).removeClass('visible');
+		$('.form-panel.one').removeClass('hidden');
+		$('.form-panel.two').removeClass('active');
+		$('.form').animate({
+		  'height': panelOne
+		}, 200);
+	  });
+	});	
+	</script>
+</head>
+<body>
+<div class="form">
+  <div class="form-toggle"></div>
+  <div class="form-panel one">
+    <div class="form-header">
+      <h1>Account Login</h1>
+    </div>
+    <div class="form-content">
+      <form>
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input type="text" id="username" name="username" required="required"/>
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" id="password" name="password" required="required"/>
+        </div>
+        <div class="form-group">
+          <label class="form-remember">
+            <input type="checkbox"/>Remember Me
+          </label><a class="form-recovery" href="#">Forgot Password?</a>
+        </div>
+        <div class="form-group">
+          <button type="submit">Log In</button>
+        </div>
+      </form>
+    </div>
+  </div>
+  <div class="form-panel two">
+    <div class="form-header">
+      <h1>Register Account</h1>
+    </div>
+    <div class="form-content">
+      <form>
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input type="text" id="username" name="username" required="required"/>
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" id="password" name="password" required="required"/>
+        </div>
+        <div class="form-group">
+          <label for="cpassword">Confirm Password</label>
+          <input type="password" id="cpassword" name="cpassword" required="required"/>
+        </div>
+        <div class="form-group">
+          <label for="email">Email Address</label>
+          <input type="email" id="email" name="email" required="required"/>
+        </div>
+        <div class="form-group">
+          <button type="submit">Register</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+<div class="pen-footer"><a href="https://www.behance.net/gallery/30478397/Login-Form-UI-Library" target="_blank"><i class="material-icons">arrow_backward</i>View on Behance</a><a href="https://github.com/andyhqtran/UI-Library/tree/master/Login%20Form" target="_blank">View on Github<i class="material-icons">arrow_forward</i></a></div>
+</body> 
+</html>
